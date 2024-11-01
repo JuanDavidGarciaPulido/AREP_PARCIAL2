85435f628be83e0f5794f398e3e2ad685f0eba2f
Juan David García Pulido <juan.david.garcia.pulido@gmail.com>
2024-11-01 03:00:04 -0500

    avance

--- src/main/java/org/example/MathServices.java
+++ src/main/java/org/example/MathServices.java
@@ -1,36 +1,80 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.Collections;
-import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MathServices {
 
-    public static int LinealSearch(List<Integer> chainList, int value){
-        for(int i = 0; i < chainList.size(); i++){
-            if(chainList.get(i).equals(value)){
+
+    @GetMapping("/linearsearch")
+    public ResponseEntity<Map<String, Object>> linearSearch (@RequestParam int[] list, @RequestParam int value){
+        int result = -1;
+        result = linearSearchMethod(list, value);
+        Map<String, Object> response = new HashMap<>();
+        response.put("operation", "linearSearch");
+        response.put("list", list);
+        response.put("value", value);
+        response.put("output", result);
+
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/binarysearch")
+    public ResponseEntity<Map<String, Object>> binarySearch (@RequestParam int[] list, @RequestParam int value){
+        int result = -1;
+        Arrays.sort(list);
+        result = binarySearchMethod(list, value);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("operation", "BinarySearch");
+        response.put("list", list);
+        response.put("value", value);
+        response.put("output", result);
+
+        return ResponseEntity.ok(response);
+
+    }
+
+    private static int linearSearchMethod(int[] chainList, int value) {
+        for (int i = 0; i < chainList.length; i++){
+            if(chainList[i]==value){
                 return i;
             }
         }
         return -1;
     }
 
-    public static int BinarySearch(List<Integer> chainList, int value){
+    private static int binarySearchMethod(int[] chainList, int value) {
         int inicialIndex = 0;
-        int finalIndex = chainList.size() - 1;
-        Collections.sort(chainList);
-        while(inicialIndex <= finalIndex) {
+        int finalIndex = chainList.length -1;
 
-            int middleIndex = inicialIndex + (finalIndex - inicialIndex) / 2;
+        while (inicialIndex <= finalIndex){
+            int middleIndex = inicialIndex + (finalIndex - inicialIndex)/2;
 
-            if (chainList.get(middleIndex) == value) {
+            if (chainList[middleIndex] == value){
                 return middleIndex;
-            } else if (chainList.get(middleIndex) < value) {
+            }else if (chainList[middleIndex] < value){
                 inicialIndex = middleIndex + 1;
-            } else {
-                finalIndex = middleIndex - 1;
+            }else{
+                finalIndex = middleIndex -1;
             }
         }
+
         return -1;
+
     }
+
+
+
+
 }
+
--- src/main/java/org/example/ProxyService.java
+++ src/main/java/org/example/ProxyService.java
@@ -1,6 +1,31 @@
 package org.example;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/proxy")
 public class ProxyService {
 
+    private final HttpConnectionExample httpConnectionExample;
+
+    public ProxyService(HttpConnectionExample httpConnectionExample){
+        this.httpConnectionExample = httpConnectionExample;
+    }
+
+    @GetMapping("/linearsearch")
+    public ResponseEntity<String> linearsearch (@RequestParam int[] list, @RequestParam int value){
+        return httpConnectionExample.linearSearch(list, value);
+    }
+
+    @GetMapping("/binarysearch")
+    public ResponseEntity<String> binarySearch (@RequestParam int[] list, @RequestParam int value){
+        return httpConnectionExample.binarySearch(list, value);
+    }
 
 }
